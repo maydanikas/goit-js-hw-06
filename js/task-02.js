3128d1e7fbcd97831eb0ee2232e7f4a93c114c93
maydanikas <123328755+maydanikas@users.noreply.github.com>
2023-03-22 22:14:24 +0100

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ul = document.getElementById("ingredients");
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const li = document.createElement("li");
+  li.textContent = ingredients[i];
+  li.classList.add("item");
+  ul.append(li);
+  console.log(li);
+}
