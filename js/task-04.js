eab3e55440622323c106e633a81b0d2e2e8072ad
maydanikas <123328755+maydanikas@users.noreply.github.com>
2023-03-23 21:30:08 +0100

    task 4

---
+++ js/task-04.js
@@ -0,0 +1,17 @@
+let counterValue = 0;
+
+const decrementBtn = document.querySelector('button[data-action="decrement"]');
+const incrementBtn = document.querySelector('button[data-action="increment"]');
+const valueEl = document.querySelector("#value");
+let counter = 0;
+
+decrementBtn.addEventListener("click", function () {
+  counterValue -= 1;
+  valueEl.textContent = counterValue;
+  return counterValue;
+});
+incrementBtn.addEventListener("click", function () {
+  counterValue += 1;
+  valueEl.textContent = counterValue;
+  return counterValue;
+});
