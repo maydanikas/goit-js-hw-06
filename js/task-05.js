62dec6dac8577268220ae2dca7a81d0dc811f4df
maydanikas <123328755+maydanikas@users.noreply.github.com>
2023-03-25 11:39:48 +0100

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,13 @@ const inputEl = document.querySelector("#name-input");
 const usernameElOutput = document.querySelector("#name-output");
 
 inputEl.addEventListener("input", (event) => {
-  if (inputEl.value === "") {
+  if (!event.currentTarget.value) {
     usernameElOutput.innerHTML = "Anonymous";
     return;
   }
   usernameElOutput.innerHTML = inputEl.value;
+  console.log(event.currentTarget.value);
 });
+
+// 5. inputEl.value === "" - це ми можемо замінити на!inputEl.value. (2 частина)
+//  В тебе є подія тобі не потрібно зовні брати inputEl в тебе для цього є currentTarget
